5b88dbd196d1c94b5e20ec0658be0c8bd50c3ab3
RuuushB <w.wait@yandex.ru>
2022-07-25 15:57:51 +0300

    make some changes to the first_name

--- first_script.py
+++ first_script.py
@@ -9,7 +9,7 @@ try:
     browser.get(link)
 
     input1 = browser.find_element(By.TAG_NAME, "input")
-    input1.send_keys("Ivan")
+    input1.send_keys("Vova")
     input2 = browser.find_element(By.NAME, "last_name")
     input2.send_keys("Petrov")
     input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
